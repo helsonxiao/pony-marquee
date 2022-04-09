1b71929f4ed4abaf8c76c091ee426d285bb51162
helsonxiao <helsonxiao@gmail.com>
2022-04-09 20:58:04 +0800

    feat: cra example

---
+++ examples/cra/src/App.js
@@ -0,0 +1,26 @@
+import { PonyMarquee } from 'pony-marquee-react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="App-text">
+          <PonyMarquee text="Edit src/App.js and save to reload." />
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
