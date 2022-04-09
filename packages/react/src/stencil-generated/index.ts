44049f9997140235b8f705e9ea66908ecca0d332
helsonxiao <helsonxiao@gmail.com>
2022-04-09 19:27:58 +0800

    feat: react wrapper

---
+++ packages/react/src/stencil-generated/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'pony-marquee';
+
+import { defineCustomElements } from 'pony-marquee/loader';
+
+defineCustomElements();
+export const PonyMarquee = /*@__PURE__*/createReactComponent<JSX.PonyMarquee, HTMLPonyMarqueeElement>('pony-marquee');
--- packages/wc/stencil.config.ts
+++ packages/wc/stencil.config.ts
@@ -1,8 +1,14 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget as react } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'pony-marquee',
   outputTargets: [
+    react({
+      componentCorePackage: 'pony-marquee',
+      proxiesFile: '../react/src/stencil-generated/index.ts',
+      includeDefineCustomElements: true,
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
